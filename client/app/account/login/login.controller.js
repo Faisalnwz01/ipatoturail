220bf461d83ecf4efc66dcb93e29d28f55fde890
GrantSchoffelen <grant.schoffelen@gmail.com>
2015-02-27 16:55:10 -0500

    true vault working swagger

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -3,10 +3,47 @@
 angular.module('babyDoctorApp')
   .controller('LoginCtrl', function ($scope, Auth, $location, $window, $http, catalyze) {
 
-var apikey = Config.catalyze_apikey;
-    var host = "https://api.catalyze.io";
 
+var apiKey = "f9fa5cdf-2de8-4ba3-9a0d-0bd12a8b4518"
+var vaultId = "d66fc65c-6d22-41f9-953a-612c45c7082e"
+var json = 'sadfasfsfsfafs'
 
+var doc = {
+ method: 'POST',
+ url: "https://api.truevault.com/v1/vaults/"+ vaultId+"/documents/",
+ headers: { 
+  'Authorization' : 'Basic ' + btoa(apiKey + ":"), 
+  'Content-Type': 'multipart/form-data'
+ },
+  params: { document: btoa(JSON.stringify(json))}
+}
+
+var get = {
+ method: 'GET',
+ url: "https://api.truevault.com/v1/vaults/"+ vaultId + "/documents/",
+ headers: {
+   'Authorization' : 'Basic ' + btoa(apiKey + ":"), 
+   'Content-Type': 'multipart/form-data'
+ }
+
+}
+
+var req = {
+ method: 'POST',
+ url: 'https://api.truevault.com/v1/users',
+ headers: {
+  'Authorization' : 'Basic ' + btoa(apiKey + ":"), 
+   'Content-Type': 'multipart/form-data'
+ },
+
+params: {username: 'asdfasfd', 
+         'password': 'swagasgsdf', 
+        }
+}
+
+$http(req).success(function(res){console.log(res)}).error(function(err){console.log(err)});
+$http(doc).success(function(data){console.log(data)}).error(function(data){console.log(data)});
+// $http(get).success(function(resy){console.log(resy)}).error(function(data){console.log(data)});
 
 
 
