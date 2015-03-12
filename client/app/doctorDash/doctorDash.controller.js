72e20daf9a4315b0063aac07699950b497ac16d6
faisal <faisalnwz01@gmail.com>
2015-03-12 15:32:16 -0400

    swag

--- client/app/doctorDash/doctorDash.controller.js
+++ client/app/doctorDash/doctorDash.controller.js
@@ -32,6 +32,10 @@ angular.module('babyDoctorApp')
 
             $http.get('api/orders').then(function(data) {
                 console.log(data.data, "what we get back from the order get call")
+                console.log(data.data[0].doctor_id, "data.data[0].doctor_id")
+                $scope.trueVaultDocId = data.data[0].document_id
+                console.log($scope.trueVaultDocId, '$scope.truevaultdocid')
+                console.log($scope.getCurrentUser.address.phone 'get current user.address.phone')
                 for (var i = 0; i < data.data.length; i++) {
                     if (data.data[i].doctor_id === $scope.getCurrentUser.address.phone) {
                       console.log('data.data', data.data[i])
