fc6c84a6b8a9da4eb9505b5fd02cd758c1e91200
GrantSchoffelen <grant.schoffelen@gmail.com>
2015-03-15 17:16:31 -0400

    pushing live

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -2,10 +2,12 @@
 
 angular.module('babyDoctorApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+    $scope.menu = [
+    // {
+    //   'title': 'Home',
+    //   'link': '/'
+    // }
+    ];
 
     $scope.isCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn;
